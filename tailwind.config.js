484c2d74e881737cb5a46059da6cdb09bbbd5a23
CinzadeNeve <ticlaudioalves@gmail.com>
2024-09-25 21:10:45 -0300

    feat: tela login

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,26 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      inter: ['Inter', 'system-ui'],
+    },
+    colors: {
+      
+      /* Cores Primárias */
+      'primary-pure': '#10B978',
+
+      /* Cores Neutrais */
+      'neutral-light': '#637083', 
+      'neutral-dark': '#09090B',
+      'neutral-pure': '#344051',
+
+      /* Cores Erro */
+      'erro-pure': '#DC2626',
+
+      /** Cores White e Black */
+      'white': '#ffffff',
+      'black': '#000000',
+    },
     extend: {},
   },
   plugins: [],
