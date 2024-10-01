fc88be0bb4e065828b151670067324144f200ed9
CinzadeNeve <ticlaudioalves@gmail.com>
2024-09-30 21:37:31 -0300

    fix: ajustes na tela

---
+++ src/app/laboratorio/ui/item_tabs.tsx
@@ -0,0 +1,17 @@
+
+import { TabsTrigger } from "@/components/ui/tabs"
+
+type type_item = {
+    titulo:string,
+    value:string,
+};
+
+const Item_Tabs = (props:type_item) =>{
+    return(
+        <>
+            <TabsTrigger className="p-[4px_12px] text-[#71717A]/60 data-[state=active]:text-[#09090B] bg-[#F4F4F5] data-[state=active]:bg-[transparent]" value={props.value}>{props.titulo}</TabsTrigger>
+        </>
+    )
+}
+
+export default Item_Tabs;
