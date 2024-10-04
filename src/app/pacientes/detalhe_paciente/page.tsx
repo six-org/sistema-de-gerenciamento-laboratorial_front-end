6ce570ef04b75e343f096aa816c555da3377919c
Anabel Marinho <anabel.soares2001@gmail.com>
2024-10-04 09:54:25 -0300

    Feat: breadcrrumb

--- src/app/pacientes/detalhe_paciente/page.tsx
+++ src/app/pacientes/detalhe_paciente/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import SessionWrapper from "@/app/components/SessionWrapper";
 import NavegacaoUser from "../../../../components/navegacaoUser";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { FiFileText } from "react-icons/fi";
 
 const detalhe_paciente = () => {
   const steps = [
@@ -18,29 +21,57 @@ const detalhe_paciente = () => {
     <>
       <div className="grid grid-cols-[17rem_1fr_23.75rem]">
         <NavegacaoUser />
-        <div className="flex flex-col ml-[50px] mt-[166px]">
-          <p className="text-[18px] font-semibold text-[#48505E]">Rastreio da Amostra</p>
-          <p className="text-[14px] font-medium text-[#667085]">Onde está a sua amostra agora? Veja aqui!</p>
 
-          <div className="w-[1016px] h-[65px] mt-5 flex">
-            <div className="absolute top-1/2 left-0 right-0 h-[2px] bg-gray-300"></div>
+      
+        <div className="flex flex-col">
+       
+        <Breadcrumb className="mt-[74px] ml-[40px] py-3">
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink
+                  href="/pacientes"
+                  className="font-medium text-[1.5rem] flex gap-[0.625rem] items-center text-primary-pure"
+                >
+                  <FiFileText />
+                  Pacientes
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="text-primary-pure" />
+              <BreadcrumbItem>
+                <BreadcrumbLink
+                  className="font-medium text-[1.5rem] text-[#48505E]"
+                >
+                  Andrey Sabino
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
 
-            <div className="flex w-full">
-              {steps.map((step, index) => (
-                <div key={index} className="flex flex-col w-[calc((100%-10px)/8)] h-[65px]">
-                  <p className="text-[16px] font-bold">{step.date}</p>
-                  <p className="text-[13px] text-[#AAAAAA]">{step.label}</p>
-                  <div className="flex items-center gap-1 p-2 ml-1"> 
-                    <div className={`w-[10px] h-[10px] rounded-full ${step.completed ? 'bg-[#4870FF]' : 'bg-[#CED2DA]'}`}></div>
-                    <div className={`w-[95px] h-[2px] rounded-[50px] ${step.completed ? 'bg-[#4870FF]' : 'bg-[#CED2DA]'}`}></div>
-                    
+          <div className="flex flex-col ml-[50px] mt-[2.75rem]">
+            <p className="text-[18px] font-semibold text-[#48505E]">Rastreio da Amostra</p>
+            <p className="text-[14px] font-medium text-[#667085]">Onde está a sua amostra agora? Veja aqui!</p>
+
+            <div className="w-[1016px] h-[65px] mt-5 flex">
+              <div className="absolute top-1/2 left-0 right-0 h-[2px] bg-gray-300"></div>
+
+              <div className="flex w-full">
+                {steps.map((step, index) => (
+                  <div key={index} className="flex flex-col w-[calc((100%-10px)/8)] h-[65px]">
+                    <p className="text-[16px] font-bold">{step.date}</p>
+                    <p className="text-[13px] text-[#AAAAAA]">{step.label}</p>
+                    <div className="flex items-center gap-1 p-2 ml-1"> 
+                      <div className={`w-[10px] h-[10px] rounded-full ${step.completed ? 'bg-primary-pure' : 'bg-[#CED2DA]'}`}></div>
+                      <div className={`w-[95px] h-[2px] rounded-[50px] ${step.completed ? 'bg-primary-pure' : 'bg-[#CED2DA]'}`}></div>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
+
       </div>
+
     </>
   );
 };
